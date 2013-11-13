4ce567208e763d5007a532a12d885d605ea6285b
Eric Horacek <horacek.eric@gmail.com>
2013-11-13 10:53:53 -0700

    Adds pod folder to example to make running the example project easier

---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MSDynamicsDrawerViewController
+#define COCOAPODS_POD_AVAILABLE_MSDynamicsDrawerViewController
+#define COCOAPODS_VERSION_MAJOR_MSDynamicsDrawerViewController 1
+#define COCOAPODS_VERSION_MINOR_MSDynamicsDrawerViewController 0
+#define COCOAPODS_VERSION_PATCH_MSDynamicsDrawerViewController 0
+
